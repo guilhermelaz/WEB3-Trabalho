c3d8ba7a9d6428a2b27fc7ac4a8ee66b6e5325db
Guilherme Lazarotto <guilherme_laz@hotmail.com>
2023-05-31 22:36:30 -0300

    Initial commit

---
+++ src/main/java/com/example/trabweb3/model/Pet.java
@@ -0,0 +1,23 @@
+package com.example.trabweb3.model;
+
+public class Pet {
+    private int identificacao;
+    private String nome;
+
+
+    public int getIdentificacao() {
+        return identificacao;
+    }
+
+    public void setIdentificacao(int identificacao) {
+        this.identificacao = identificacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
